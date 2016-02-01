d1c5a24e51605ab5040196709e24e82254cda6c2
Joe Stubbs <fastkite@yahoo.com>
2016-01-31 21:43:33 -0500

    submitted

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -3,16 +3,15 @@
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
-
 /* We're placing all of our tests within the $() function,
  * since some of these tests may require DOM elements. We want
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
     /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+     * a related set of tests. This suite is all about the RSS
+     * feeds definitions, the allFeeds variable in our application.
+     */
     describe('RSS Feeds', function() {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
@@ -31,8 +30,8 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it ('urls defined',function() {
-            allFeeds.forEach(function (feed){ // loop through each feed
+        it('urls defined', function() {
+            allFeeds.forEach(function(feed) { // loop through each feed
                 expect(feed.url).toBeDefined(); // url must be defined
                 expect(feed.url).not.toBe(null); // url must not be null
                 expect(feed.url.length).not.toBe(0); // length must not be 0 length string
@@ -44,13 +43,13 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         it ('name defined',function() {
-            allFeeds.forEach(function (feed){ // loop through each feed
+        it('name defined', function() {
+            allFeeds.forEach(function(feed) { // loop through each feed
                 expect(feed.name).toBeDefined(); // url must be defined
                 expect(feed.name).not.toBe(null); // url must not be null
                 expect(feed.name.length).not.toBe(0); // length must not be 0 length string
             });
-         });
+        });
 
 
 
@@ -64,15 +63,15 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-        it ('to be hidden on page load',function() {
+        it('to be hidden on page load', function() {
             // lets make sure the menu-hidden class is applied to the body when the page loads
             expect($('body').hasClass('menu-hidden')).toBe(true);
 
             // lets make sure the
             var transform = $('.slide-menu').css('transform');
             // split the matrix into values
-            var values = transform.split('(')[1],
-            values = values.split(')')[0],
+            var values = transform.split('(')[1];
+            values = values.split(')')[0];
             values = values.split(',');
             // the 4th value in the matrix is the X transform off the screen to the left, should be a negative number
             expect(values[4]).toBeLessThan(0);
@@ -84,20 +83,18 @@ $(function() {
          * clicked and does it hide when clicked again.
          */
 
-        it ('to unhidden/hide when menu icon clicked twice',function() {
-
-
+        it('to unhidden/hide when menu icon clicked twice', function() {
 
             var menuicon = $('.menu-icon-link');
 
             // click the menu button to display the menu
-            menuicon.trigger( "click" );
+            menuicon.trigger("click");
             // lets make sure the menu-hidden class is no longer on the body
             expect($('body').hasClass('menu-hidden')).toBe(false);
 
 
             // click the menu button to hide the menu
-            menuicon.trigger( "click" );
+            menuicon.trigger("click");
             // lets make sure the menu-hidden class is no longer on the body
             expect($('body').hasClass('menu-hidden')).toBe(true);
 
@@ -113,19 +110,27 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+        it('should have at least 1 feeds', function() {
+            expect(allFeeds.length).toBeGreaterThan(0);
+        });
 
-        beforeEach (function(done) {
-            // load the 0th feed
-            loadFeed(0,function(){
+        if (allFeeds.length >= 1) {
+            beforeEach(function(done) {
+                // load the 0th feed
+                loadFeed(0, function() {
+                    done();
+                });
+            });
+
+            it('should load at least one entry', function(done) {
+                // verify that the .feed class has at least 1 .entry class within in
+                expect($('.feed').find('.entry').length).toBeGreaterThan(0);
+
+                // make sure the header is loaded with the feed name
+                expect($('.header-title').html()).toBe(allFeeds[0].name);
                 done();
             });
-        });
-
-        it('should load at least one entry',function(done) {
-            // verify that the .feed class has at least 1 .entry class within in
-            expect($('.feed').find('.entry').length).toBeGreaterThan(0);
-            done();
-        });
+        }
 
 
     });
@@ -136,38 +141,45 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        var emptyHTML = $('.feed').html();
-        var beforeHTML;
-        var afterHTML;
 
-        beforeEach (function(done) {
-            // load the 0th feed
-            loadFeed(0,function(){
-                beforeHTML = $('.feed').html();
+        it('should have at least 2 feeds', function() {
+            expect(allFeeds.length).toBeGreaterThan(1);
+        });
+        // we can only run the following tests if we have enough feeds
+        if (allFeeds.length >= 2) {
+
+            var emptyHTML = $('.feed').html();
+            var beforeHTML;
+            var afterHTML;
+
+            beforeEach(function(done) {
+                // load the 0th feed
+                loadFeed(0, function() {
+                    beforeHTML = $('.feed').html();
+                    done();
+                });
+            });
+
+
+            beforeEach(function(done) {
+                // load the 0th feed
+                loadFeed(1, function() {
+                    afterHTML = $('.feed').html();
+                    done();
+                });
+            });
+
+            it('should change the contents of the feed', function(done) {
+                // before and after HTMLs must not be equal to an empty one
+                expect(emptyHTML).not.toBe(beforeHTML);
+                expect(emptyHTML).not.toBe(afterHTML);
+
+                // verify that the .feed class changes before and after
+                expect(afterHTML).not.toBe(beforeHTML);
                 done();
             });
-        });
 
-
-        beforeEach (function(done) {
-            // load the 0th feed
-            loadFeed(1,function(){
-                afterHTML = $('.feed').html();
-                done();
-            });
-        });
-
-        it('should change the contents of the feed',function(done) {
-            // before and after HTMLs must not be equal to an empty one
-            expect(emptyHTML).not.toBe(beforeHTML);
-            expect(emptyHTML).not.toBe(afterHTML);
-
-            // verify that the .feed class changes before and after
-            expect(afterHTML).not.toBe(beforeHTML);
-            done();
-        });
-
-
+        }
 
 
     });
--- js/app.js
+++ js/app.js
@@ -132,3 +132,6 @@ $(function() {
         $('body').toggleClass('menu-hidden');
     });
 }());
+
+
+
